7e9b06095ee83a5a3a6a397eb8c46facb74db1ea
Irfan Mohammad <145257683+irfan-md-01@users.noreply.github.com>
2025-12-18 18:05:14 +0530

    Adding basic security

--- CodeBox/settings.py
+++ CodeBox/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-az1279@w2!6bqn3k8q3$cpa5*^#ip1sl432dq)rp*#%^u*$bah'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["codebox-2-0.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
